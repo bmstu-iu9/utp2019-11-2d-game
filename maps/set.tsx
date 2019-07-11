6af2ff21880ecc9af297bdf4e49df94ff57a9968
Fumi Hin <fumihin@outlook.com>
2019-07-11 19:07:07 +0300

    Карта на тест подгручику, будет перерисована

--- maps/set.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="set" tilewidth="1" tileheight="1" tilecount="10752" columns="112">
- <image source="../../Pictures/set.png" width="112" height="96"/>
-</tileset>
