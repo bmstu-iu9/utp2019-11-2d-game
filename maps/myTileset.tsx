6b0322446b9dcb2ee0ca738cf2cc0b03dbef3451
Fumi Hin <fumihin@outlook.com>
2019-08-04 18:19:29 +0300

    Простая карта без коллижнов

---
+++ maps/myTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="myTileset" tilewidth="32" tileheight="32" tilecount="1927" columns="41">
+ <image source="dawn_of_the_gods.png" trans="4c5a8e" width="1312" height="1504"/>
+</tileset>
