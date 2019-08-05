c1446f3ed6ac7e217e053a6fc2ac39f32517aed6
Fumi <fumihin@outlook.com>
2019-08-05 15:50:24 +0300

    Прозрачность тайлов, Бэкграунд из тайлов

--- maps/lightblockShadowed.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="lightblockShadowed" tilewidth="1" tileheight="1" tilecount="300" columns="15">
- <image source="lightblockShadowed.png" width="15" height="20"/>
-</tileset>
