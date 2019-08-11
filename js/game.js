39349da16d93296ad2ad699625625e4424e1f404
tmible <panevnikita@inbox.ru>
2019-08-11 19:31:52 +0300

    Сделал статическое вращение (вращение только на месте) обоих кораблей. Поменял названия кораблей (были наоборот).

--- js/game.js
+++ js/game.js
@@ -13,27 +13,77 @@ wedge.src = "img/wedge.png";
 center.src = "img/center.png";
 
 document.addEventListener("keydown", move);
-
 function move(e) {
-  //alert(e.keyCode);
-  switch(e.keyCode){
-    //wedge
-    case 65: // KeyA - left
-    break;
-    case 68: // KeyD - right
-    break;
-    case 87: // KeyW - bang
-    break;
-    case 83: // KeyS - nitro
-    break;
-    //needle
-  }
+    switch(e.keyCode){
+        //wedge
+        case 65: // KeyA - left
+            ctx.clearRect(0,0,800,800);
+            ctx.drawImage(bg, 0, 0);
+            ctx.save();
+            ctx.translate(22,22);
+            ctx.rotate(wA-Math.PI/10);
+            wA-=Math.PI/10;
+            ctx.translate(-22,-22);
+            ctx.drawImage(wedge, wX,wY);
+            ctx.restore();
+            ctx.drawImage(needle, nX, nY);
+            ctx.drawImage(center, cX,cY);
+            break;
+        case 68: // KeyD - right
+            ctx.clearRect(0,0,800,800);
+            ctx.drawImage(bg, 0, 0);
+            ctx.save();
+            ctx.translate(22,22);
+            ctx.rotate(wA+Math.PI/10);
+            wA+=Math.PI/10;
+            ctx.translate(-22,-22);
+            ctx.drawImage(wedge, wX,wY);
+            ctx.restore();
+            ctx.drawImage(needle, nX, nY);
+            ctx.drawImage(center, cX,cY);
+            break;
+        case 87: // KeyW - bang
+            break;
+        case 83: // KeyS - nitro
+            break;
+        //needle
+        case 74: // KeyJ - left
+            ctx.clearRect(0,0,800,800);
+            ctx.drawImage(bg, 0, 0);
+            ctx.save();
+            ctx.translate(778,778);
+            ctx.rotate(nA-Math.PI/10);
+            nA-=Math.PI/10;
+            ctx.translate(-778,-778);
+            ctx.drawImage(needle, nX, nY);
+            ctx.restore();
+            ctx.drawImage(wedge, wX,wY);
+            ctx.drawImage(center, cX,cY);
+            break;
+        case 76: // KeyL - right
+            ctx.clearRect(0,0,800,800);
+            ctx.drawImage(bg, 0, 0);
+            ctx.save();
+            ctx.translate(778,778);
+            ctx.rotate(nA+Math.PI/10);
+            nA+=Math.PI/10;
+            ctx.translate(-778,-778);
+            ctx.drawImage(needle, nX, nY);
+            ctx.restore();
+            ctx.drawImage(wedge, wX,wY);
+            ctx.drawImage(center, cX,cY);
+            break;
+        case 73: // KeyI - bang
+            break;
+        case 75: // KeyK - nitro
+            break;
+    }
 }
 
 
 
-var nX = 10, nY = 10;
-var wX = 765, wY = 765;
+var wX = 10, wY = 10, wA = 0;
+var nX = 765, nY = 765, nA = 0;
 var cX = 392, cY = 392;
 //var gravity = 0.5;
 function draw() {
@@ -43,8 +93,7 @@ function draw() {
   ctx.drawImage(center, cX,cY);
   //nX+=gravity;
   //nY+=gravity;
-
-  requestAnimationFrame(draw);
+  //requestAnimationFrame(draw);
 }
 
-wedge.onload = draw;
+center.onload = draw;
